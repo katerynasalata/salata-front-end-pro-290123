77a3608d5d290cec06ea6604399d0583a15eb7bf
Kateryna Salata <14katrin12@gmail.com>
2023-02-12 00:34:56 +0100

    Homework 3

---
+++ Homework_3/hw3_1/index.js
@@ -0,0 +1,4 @@
+let name = prompt('What is your name?');
+let num = prompt('What is your favorite number?');
+let fruit = prompt('What is your favourite fruit?');
+alert(name + num + fruit);
---
+++ Homework_3/hw3_2/index.js
@@ -0,0 +1,3 @@
+let num = 12345;
+let digits = num.toString().split('');
+console.log(digits[0] + ' ' + digits[1] + ' ' + digits[2] + ' ' + digits[3] + ' ' + digits[4]);
