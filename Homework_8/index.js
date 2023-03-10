f0dc463dfc0a4c9e7dca44749f3f863ac870402c
Kateryna Salata <14katrin12@gmail.com>
2023-03-10 13:21:33 +0100

    update to hw-8

--- Homework_8/index.js
+++ Homework_8/index.js
@@ -1,11 +1,11 @@
 let year, city, sport, country, cityMessage, age, sportMessage, athlete, ageMessage;
 
-year = prompt('What year were you born?');
+year = +prompt('What year were you born?');
 city = prompt('What city do you live in?');
 sport = prompt('What is your favorite sport?');
 
 if (year) {
-    age = 2023 - Number(year);
+    age = 2023 - year;
     ageMessage = 'You are ' + age + ' years old.';
 }
 else {
@@ -14,23 +14,16 @@ else {
 
 if (city) {
     if (city == 'Kyiv') {
-        country = 'Ukraine';
+        cityMessage = 'You live in the capital of Ukraine';
     }
     else if (city == 'Washington') {
-        country = 'USA';
+        cityMessage = 'You live in the capital of USA';
     }
     else if (city == 'London') {
-        country = 'England';
+        cityMessage = 'You live in the capital of England';
     }
     else {
-        country = false;
-    }
-    
-    if (country == false) {
-        cityMessage = 'You live in ' + city + '.';
-    }
-    else {
-        cityMessage = 'You live in the capital of ' + country + '.';
+        cityMessage = 'You live in ' + city + '.'; 
     }
 }
 else {
@@ -39,16 +32,17 @@ else {
 
 if (sport) {
     if(sport == 'Football') {
-        athlete = 'Lionel Messi';
+        sportMessage = 'Cool! Do you want to become Lionel Messi?';
     }
     else if(sport == 'Tennis') {
-        athlete = 'Serena Williams';
+        sportMessage = 'Cool! Do you want to become Serena Williams?';
     }
     else if(sport == 'Boxing') {
-        athlete = 'Volodymyr Klychko';
+        sportMessage = 'Cool! Do you want to become Volodymyr Klychko?';
     }
-
-    sportMessage = 'Cool! Do you want to become ' + athlete + '?';
+    else {
+        sportMessage = 'Your favourite sport is ' + sport;
+    }
 }
 else {
     sportMessage = "It's a pity that you didn't share with us your favorite sport.";
